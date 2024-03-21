8f7b89435a45bd463f9092a375c01c505c685b4f
Ahmed_GitHub <ahmedeltantawy06@gmail.com>
2024-03-21 03:20:51 +0200

    End of models Part

---
+++ ECommerce/Data/ECommerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Data
+{
+    public class ECommerceDbContext:DbContext
+    {
+        public ECommerceDbContext(DbContextOptions<ECommerceDbContext>  options) :base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ECommerce/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        //Navigational Property
+        public ICollection<Product> Products { get; set; }
+    }
+}
